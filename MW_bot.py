12d2c01f36360ac0136a9e61ce797a0de76f7c83
Jingyang Fan <jingyangfan@Jingyangs-MacBook-Pro.local>
2020-02-05 23:25:23 -0500

    changes to code

--- MW_bot.py
+++ MW_bot.py
@@ -6,10 +6,16 @@ class MW_bot:
     def __init__(self):
         self.driver = webdriver.Chrome()
     
-    def login(self):
-        self.driver.get('https://sso.accounts.dowjones.com/login?state=g6Fo2SBleG1tbHpGVG55Wm9xdFczM0FoT3RBamw1MWRxMXExTaN0aWTZIDdmY1Y5aGdMMGRRQUZsME1UNVJIRW5Md0EzY3VOT1Eto2NpZNkgNWhzc0VBZE15MG1KVElDbkpOdkM5VFhFdzNWYTdqZk8&client=5hssEAdMy0mJTICnJNvC9TXEw3Va7jfO&protocol=oauth2&scope=openid%20idp_id%20given_name%20family_name%20email%20djid%20prts&response_type=code&redirect_uri=https%3A%2F%2Faccounts.marketwatch.com%2Fauth%2Fsso%2Flogin&nonce=6903a150-8354-4330-9a31-97e50041293b&ui_locales=en-us-x-mw-3-8&ns=prod%2Faccounts-mw&savelogin=on#!/signin')
-        sleep(0.5)
+    def login(self, game_name):
+        game_name = game_name.lower()
+        game_name = game_name.replace(' ', '-')
+        self.driver.get('https://www.marketwatch.com/game/' + game_name)
 
+        # get to the log in screen
+        login_button = self.driver.find_element_by_xpath('/html/body/section/div[1]/header/div/div/a[2]/span')
+        login_button.click()
+
+        # input username and password then click submit
         userid_in = self.driver.find_element_by_xpath('//*[@id="username"]')
         password_in = self.driver.find_element_by_xpath('//*[@id="password"]')
         submit_button = self.driver.find_element_by_xpath('//*[@id="basic-login"]/div[1]/form/div/div[6]/div[1]/button')
@@ -17,6 +23,6 @@ class MW_bot:
         userid_in.send_keys(username)
         password_in.send_keys(password)
         submit_button.click()
-
+    
 bot = MW_bot()
-bot.login()
+bot.login('NO CORONA')
